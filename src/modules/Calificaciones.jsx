140959ce7f972c3b6185e0c2ada198772474dc50
NoePerezFab <noeperez@e-gisnet.com>
2022-03-10 00:47:21 -0600

    Version 1 de frontend de calificador de eventos

---
+++ src/modules/Calificaciones.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import SockJsClient from 'react-stomp';
+
+const Calificaciones = () => {
+  const SOCKET_URL = 'https://calificador-eventos.herokuapp.com/ws'
+  const [ejecucion, setejecucion] = useState([])
+  const [impresion, setimpresion] = useState([])
+  const [dificultad, setdificultad] = useState([])
+  const onMessageReceived = (m) => {
+    console.log(m);
+    setejecucion(m.ejecucion)
+    setimpresion(m.impresionArtistica)
+    setdificultad(m.dificultad)
+  }
+  return (
+    <div className='container d-flex justify-content-center align-items-center h-100 '>
+      <div className='container  align-items-center justify-content-center  '>
+    <SockJsClient
+      url={SOCKET_URL}
+      topics={['/call/message']}
+      onConnect={console.log("Connected!")}
+      onDisconnect={console.log("Disconnected!")}
+      onMessage={msg => onMessageReceived(msg)}
+      debug={false}/>
+      <div className='row mb-5'>
+      <h3 className='mr-5'>Ejecucion</h3>
+      {ejecucion.map(juez => {
+        return(
+          <div className='text-center'>
+          <h3 className='mr-5'>{juez.nombre}</h3>
+          <h3 className='mr-5'>{juez.calificacion}</h3>
+          </div>
+        )
+      })
+      }
+      {ejecucion.length === 5 ?
+        <>
+        <div className='text-center'>
+        <h3 className='mr-5'>Promedio</h3>
+        <h3 className='mr-5'>{(ejecucion[0].calificacion+ejecucion[1].calificacion+ejecucion[2].calificacion+ejecucion[3].calificacion+ejecucion[4].calificacion)/5}</h3>
+        </div>
+        </>:<></>
+      }
+      </div>
+      
+      <div className='row mb-5'>
+      <h3 className='mr-5'>Impresion Artistica</h3>
+      {impresion.map(juez => {
+        return(
+          <div className='text-center'>
+          <h3 className='mr-5'>{juez.nombre}</h3>
+          <h3 className='mr-5'>{juez.calificacion}</h3>
+          </div>
+        )
+      })}
+      {impresion.length === 5 ?
+        <>
+        <div className='text-center'>
+        <h3 className='mr-5'>Promedio</h3>
+        <h3 className='mr-5'>{(impresion[0].calificacion+impresion[1].calificacion+impresion[2].calificacion+impresion[3].calificacion+impresion[4].calificacion)/5}</h3>
+        </div>
+        </>:<></>
+      }
+      </div>
+
+      <div className='row mb-5'>
+      <h3 className='mr-5'>Dificultad</h3>
+      {dificultad.map(juez => {
+        return(
+          <div className='text-center'>
+          <h3 className='mr-5'>{juez.nombre}</h3>
+          <h3 className='mr-5'>{juez.calificacion}</h3>
+          </div>
+        )
+      })}
+      {dificultad.length === 5 ?
+        <>
+        <div className='text-center'>
+        <h3 className='mr-5'>Promedio</h3>
+        <h3 className='mr-5'>{(dificultad[0].calificacion+dificultad[1].calificacion+dificultad[2].calificacion+dificultad[3].calificacion+dificultad[4].calificacion)/5}</h3>
+        </div>
+        </>:<></>
+      }
+      </div>
+
+      </div>
+  </div>
+  )
+}
+
+export default Calificaciones
---
+++ src/modules/Login.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom'
+
+
+const Login = () => {
+    const [red, setred] = useState(false)
+    const crearEvento = async()=>{
+        const request = {nombre : "Evento 1"}
+        const requestJson = JSON.stringify(request)
+        try{
+            const response = await fetch('https://calificador-eventos.herokuapp.com/api/nuevoevento',{ 
+                headers : { 'Content-Type': 'application/json' },
+                method: 'POST',
+                mode: 'cors', // <---
+                cache: 'default',
+                body: requestJson
+              })
+            const responseJson = await response.json()
+            if(responseJson.status === 1){
+                setred(true)
+            }
+        }
+        catch(e){
+            console.log("Error : "+e);
+        }
+        
+    }
+
+  return (
+      !red ?
+    <div className='d-flex justify-content-center align-items-center h-100'>
+        <button type='button' className='btn btn-primary' onClick={crearEvento}>Crear nuevo evento</button>
+    </div> : <Navigate to={"/calificaciones"}/>
+  )
+}
+
+export default Login
