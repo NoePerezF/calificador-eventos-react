700be64f783acfde573d0ddd1cbd0d3d06a990ff
NoePerezFab <noeperez@e-gisnet.com>
2022-05-10 17:17:53 -0500

    Prueba de fetch a evento activo

--- src/modules/EventosProgramados.jsx
+++ src/modules/EventosProgramados.jsx
@@ -28,31 +28,22 @@ const EventosProgramados = () => {
         <table class="table">
             <thead class="thead-dark">
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Fecha</th>
+                <th scope="col">Status</th>
+                <th scope="col">Acciones</th>
             </tr>
             </thead>
             <tbody>
-                <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
+                {eventos.map(e =>{
+                    <tr>
+                        <td>{e.nombre}</td>
+                        <td>{e.fecha}</td>
+                        <td>{e.estado}</td>
+                        <th scope="col">Activar</th>
+                    </tr>
+                })}
+                
   </tbody>
 </table>
     </>
